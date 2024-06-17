bf998ff9451bae8f04836a056e909e5461e03e7b
Megavardhini.P <megavardhini1958@gmail.com>
2024-06-18 00:00:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+      var p = document.createElement("p");
+        setTimeout(function() {p.innerText= '10... ';
+        setTimeout(function() {p.innerText = '9... ';
+        setTimeout(function() {p.innerText = '8... ';
+        setTimeout(function() {p.innerText = '7... ';
+        setTimeout(function() {p.innerText = '6... ';
+        setTimeout(function() {p.innerText = '5... ';
+        setTimeout(function() {p.innerText = '4... ';
+        setTimeout(function() {p.innerText = '3... ';
+        setTimeout(function() {p.innerText = '2... ';                            
+        setTimeout(function() {p.innerText= '1... ';
+        setTimeout(function() {p.innerText = 'Happy Independence Day!'; }, 1000); 
+        }, 1000);}, 1000); },1000); }, 1000);}, 1000); }, 1000); }, 1000); }, 1000); }, 1000); }, 1000); 
+document.body.append(p);
